1df4f4dc9db088fa6a4418ce39d170230c09119c
Zoyo23 <lucasoliveiraoueiroz@gmail.com>
2020-08-05 08:46:59 -0300

    adição de roteamento, definição de cabeçalho comuns e adição de assets

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-Form" className="container">
+            <PageHeader title="Que incríve que você quer dar aulas." />
+        </div>
+    )
+}
+
+export default TeacherForm;
